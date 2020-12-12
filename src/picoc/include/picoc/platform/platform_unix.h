1ac34c9939d3d61daaf5db3b613585970c6a7805
Cory Todd <cory@pyramidacceptors.com>
2020-12-11 20:43:41 -0700

    fix bad gitignore entry
    was excluding basically everything

---
+++ src/picoc/include/picoc/platform/platform_unix.h
@@ -0,0 +1,11 @@
+#include <stdint.h>
+#include <unistd.h>
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#define PICOC_SUPPORT_FCNTL
+#undef PICOC_NO_STRPTIME
+
+#define USE_READLINE
